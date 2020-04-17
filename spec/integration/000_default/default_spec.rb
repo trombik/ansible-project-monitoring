39d6a166bb542a5467bff265eae0717a2390d1be
Tomoyuki Sakurai <y@trombik.org>
2020-04-17 14:05:28 +0900

    ci: update integration test
    test login page with capybara

--- spec/integration/000_default/default_spec.rb
+++ spec/integration/000_default/default_spec.rb
@@ -1,50 +1,33 @@
 require_relative "../spec_helper"
+require "capybara/rspec"
+
+Capybara.configure do |config|
+  config.run_server = false
+  config.app_host = "http://172.16.100.254:3000"
+end
+Capybara.default_driver = :selenium_chrome_headless
+Capybara.javascript_driver = :selenium_chrome_headless
 
 case test_environment
 when "virtualbox"
-  context "after provision finishes" do
-    all_hosts_in("virtualbox").each do |s|
-      describe s do
-        it "runs `echo foo`" do
-          r = current_server.ssh_exec "echo foo"
-          expect(r).to match(/^foo$/)
-        end
-      end
+  feature "Login to sensu-backend" do
+    scenario "with vaild credential" do
+      sign_up_with "admin" , "P@ssw0rd!"
+
+      expect(page).to have_content("Last executed")
     end
-    (all_hosts_in("ns") + all_hosts_in("client")).each do |s|
-      describe s do
-        let(:dig_command) do
-          case current_server.ssh_exec "uname -s".chomp
-          when /FreeBSD/
-            "drill -t"
-          when /OpenBSD/
-            "dig"
-          else
-            raise "Unknown OS"
-          end
-        end
 
-        it "has a list of internal DNS server in /etc/resolv.conf" do
-          r = current_server.ssh_exec "cat /etc/resolv.conf"
-          all_hosts_in("ns").each do |ns_server|
-            expect(r).to match(/^nameserver\s+#{ns_server.server.address}$/)
-          end
-        end
+    scenario "with invalid credential" do
+      sign_up_with "foo", "bar"
 
-        it "resolves internal-only A RRs" do
-          yaml = YAML.load_file(playbooks_path + "group_vars" + "#{test_environment}.yml")
-          expect(yaml["project_hosts"].length).not_to eq 0
-          yaml["project_hosts"].each do |host|
-            r = current_server.ssh_exec "#{dig_command} #{host['name']}.i.trombik.org"
-            expect(r).to match(/^;; ANSWER SECTION:\n#{host['name']}\.i\.trombik\.org\.\s+86400\s+IN\s+A\s+#{host['ipv4']}$/)
-          end
-        end
+      expect(page).to have_content("Sign in")
+    end
 
-        it "resolves external RR over TCP" do
-          r = current_server.ssh_exec "#{dig_command} a example.org"
-          expect(r).to match(/(rcode|status): NOERROR/)
-        end
-      end
+    def sign_up_with(account, password)
+      visit "signin"
+      fill_in "username", with: account
+      fill_in "password", with: password
+      click_button "Sign in"
     end
   end
 end
