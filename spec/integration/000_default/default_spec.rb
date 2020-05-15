5d37260d997afc9fabd5ada0ef16fb963e708c28
Tomoyuki Sakurai <y@trombik.org>
2020-05-15 17:33:48 +0900

    imp: add some nagios-plugins checks, and readonly user

--- spec/integration/000_default/default_spec.rb
+++ spec/integration/000_default/default_spec.rb
@@ -3,6 +3,12 @@
 require_relative "../spec_helper"
 require "capybara/rspec"
 
+admin_user = credentials_yaml["sensu_go_backend_admin_account"]
+admin_pass = credentials_yaml["sensu_go_backend_admin_password"]
+
+readonly_user = credentials_yaml["project_readonly_user"]
+readonly_pass = credentials_yaml["project_readonly_pass"]
+
 Capybara.configure do |config|
   config.run_server = false
   config.app_host = "http://172.16.100.254:3000"
@@ -13,8 +19,14 @@ Capybara.javascript_driver = :selenium_chrome_headless
 case test_environment
 when "virtualbox"
   feature "Login to sensu-backend" do
-    scenario "with vaild credential" do
-      sign_up_with "admin", "P@ssw0rd!"
+    scenario "with vaild credential of admin user" do
+      sign_up_with admin_user, admin_pass
+
+      expect(page).to have_content("Last executed")
+    end
+
+    scenario "with vaild credential of readonly user" do
+      sign_up_with readonly_user, readonly_pass
 
       expect(page).to have_content("Last executed")
     end
---
+++ spec/serverspec/backend/100_backend_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative "../spec_helper"
+
+admin_user = credentials_yaml["sensu_go_backend_admin_account"]
+admin_pass = credentials_yaml["sensu_go_backend_admin_password"]
+
+describe command("sensuctl configure -n --url http://127.0.0.1:8080 --username #{Shellwords.escape(admin_user)} --password #{Shellwords.escape(admin_pass)} --format yaml") do
+  before(:all) do
+    Specinfra.backend.run_command("rm -rf /root/.config/sensu")
+  end
+  its(:exit_status) { should eq 0 }
+  case os[:family]
+  when "freebsd"
+    its(:stdout) { should match(/Using Basic Auth in HTTP mode is not secure/) }
+  else
+    its(:stdout) { should eq "" }
+  end
+  its(:stderr) { should eq "" }
+end
+
+describe command "sensuctl user list --format json" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout_as_json) { should include(include("username" => "readonly")) }
+end
