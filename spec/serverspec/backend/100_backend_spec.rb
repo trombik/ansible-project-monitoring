5239edb23f1f10c8c95334253609b225316766cc
Tomoyuki Sakurai <y@trombik.org>
2020-05-19 17:44:15 +0900

    feature: enable slack notification

--- spec/serverspec/backend/100_backend_spec.rb
+++ spec/serverspec/backend/100_backend_spec.rb
@@ -4,6 +4,8 @@ require_relative "../spec_helper"
 
 admin_user = credentials_yaml["sensu_go_backend_admin_account"]
 admin_pass = credentials_yaml["sensu_go_backend_admin_password"]
+gems = %w[sensu-plugins-slack]
+sensu_user = "sensu"
 
 describe command("sensuctl configure -n --url http://127.0.0.1:8080 --username #{Shellwords.escape(admin_user)} --password #{Shellwords.escape(admin_pass)} --format yaml") do
   before(:all) do
@@ -24,4 +26,39 @@ describe command "sensuctl check list --format json" do
   its(:exit_status) { should eq 0 }
   its(:stderr) { should eq "" }
   its(:stdout_as_json) { should include(include("metadata" => include("name" => "check_disk"))) }
+end
+
+describe command "sensuctl handler list --format json" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout_as_json) { should include(include("metadata" => include("name" => "slack"))) }
+end
+
+gems.each do |g|
+  case os[:family]
+  when "redhat"
+    describe command "/opt/sensu-plugins-ruby/embedded/bin/gem list --local" do
+      its(:stderr) { should eq "" }
+      its(:stdout) { should match(/#{g}/) }
+    end
+  else
+    describe package g do
+      let(:sudo_options) { "-u #{sensu_user} --set-home" }
+      it { should be_installed.by("gem") }
+    end
+  end
+end
+
+sensu_plugins_conf_dir = case os[:family]
+                         when "freebsd"
+                           "/usr/local/etc/sensu/conf.d"
+                         else
+                           "/etc/sensu/conf.d"
+                         end
+
+describe file "#{sensu_plugins_conf_dir}/handler-slack.json" do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 644 }
+  its(:content_as_json) { should include("slack" => include("bot_name" => "slack-plugin")) }
 end
