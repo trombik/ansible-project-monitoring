1cbc6d589af2cab0a037f286c6ce1b59713f45ce
Tomoyuki Sakurai <y@trombik.org>
2020-07-26 17:25:31 +0900

    bugfix: add target2 to weed out portability issues
    fix several portability issues, as a result.
    
    disable an assert in trombik.hostname. this should be fixed in the role.

--- spec/serverspec/shared_examples/hostname.rb
+++ spec/serverspec/shared_examples/hostname.rb
@@ -4,6 +4,7 @@ shared_examples "a host with a valid hostname" do
   describe command("hostname") do
     its(:exit_status) { should eq 0 }
     its(:stderr) { should eq "" }
-    its(:stdout) { should match(/^#{ENV["TARGET_HOST"]}$/) }
+    # XXX some OS allow to set FQDN as hostname, but others do not (Ubuntu)
+    its(:stdout) { should match(/^#{ENV["TARGET_HOST"].split(".").first}(?:\.i\.trombik\.org)?$/) }
   end
 end
